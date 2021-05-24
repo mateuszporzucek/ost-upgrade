5d6b1064310c2d1db7a8b5e513237a4293917f6c
GitHub Action <action@github.com>
2021-05-24 10:15:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mateuszporzucek/ostupgrade/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mateuszporzucek.ostupgrade.services
+
+import com.github.mateuszporzucek.ostupgrade.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
